0b40a76f451ae6a9374b334015b8cce6b32a20b4
M Shekhar Dora <63382577+Shekhar403@users.noreply.github.com>
2023-04-04 17:58:48 +0530

    simon game

---
+++ simon/index.js
@@ -0,0 +1,76 @@
+var levelCount = 1;
+var bufferCount = 0;
+var colouredButtonsArray = ["red", "blue", "yellow", "green"];
+var colorBuffer = [];
+
+$(".btn").click(function (e) { 
+   e.currentTarget.classList.add("pressed");
+   setTimeout(function() {
+    e.currentTarget.classList.remove("pressed");
+   }, 100);
+
+   if (colorBuffer.length === 0) {
+    gameOver();
+   }
+   else {
+    playAudio(this.classList[1]);
+
+    if (this.classList[1] === colorBuffer[bufferCount]) {
+        if (bufferCount === colorBuffer.length - 1) {
+            // level successful
+            levelProgress();
+        }
+        else {
+            console.log("next buffer");
+            console.log(bufferCount);
+            // awaiting next buffer
+            bufferCount += 1
+
+        }
+   }
+   else {
+    // game over
+    // Game Over, Press Any Key to Restart
+    gameOver();
+   }
+   }
+});
+
+$(document).keypress(function (e) { 
+    if ($("#level-title").text() === "Press A Key to Start" || $("#level-title").text() === "Game Over, Press Any Key to Restart") {
+        $("#level-title").text("Level " + levelCount);
+        addANewColor();
+    }
+    
+});
+
+
+function playAudio(colorName) {
+    $("." + colorName).fadeIn(100).fadeOut(100).fadeIn(100);
+    let audio = new Audio("sounds/" + colorName + ".mp3");
+    audio.play(colouredButtonsArray)
+}
+
+function gameOver() {
+    playAudio("wrong");
+    $("#level-title").text("Game Over, Press Any Key to Restart");
+    $("body").css("backgroundColor", "red");
+    setTimeout(function() {
+        $("body").css("backgroundColor", "#011F3F");
+    }, 200);
+}
+
+function levelProgress() {
+    bufferCount = 0
+    levelCount += 1;
+    $("#level-title").text("Level " + levelCount);
+    setTimeout(function() {
+        addANewColor();
+    }, 1000);
+}
+
+function addANewColor() {
+    let color = colouredButtonsArray[Math.floor(Math.random() * 4)]
+    colorBuffer.push(color);
+    playAudio(color);
+}
