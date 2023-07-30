9b299c9977ce4824d51db26662d6074debfa8cfd
M Shekhar Dora <63382577+Shekhar403@users.noreply.github.com>
2023-07-30 11:10:27 +0530

    conditional rendering

--- react/reactprojectone/src/App.js
+++ react/reactprojectone/src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
 import Home from './components/Home';
+import ShowArray from './components/ShowArray';
 
 function App() {
   let name  = "My First React App";
   let fname = "sgfg";
+  let fruits = ["Apple","Orange","Banana","Pineapple"];
 
   return (
     <div className="App">
       <Home name = {name} fname = {fname} />
+      <ShowArray fruits = {fruits}></ShowArray>
     </div>
+   
   );
 }
 
